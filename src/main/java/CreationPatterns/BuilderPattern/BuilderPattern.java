8e86adb537ed7b621d53169f3b6695a044016a34
Xiaoming Sun <rsun2014@gmail.com>
2018-02-25 09:41:18 -0800

    Builder Pattern

--- src/main/java/CreationPatterns/BuilderPattern/BuilderPattern.java
+++ src/main/java/CreationPatterns/BuilderPattern/BuilderPattern.java
@@ -2,6 +2,9 @@ package CreationPatterns.BuilderPattern;
 
 public class BuilderPattern {
     public static void main(String[] args) {
+        // See the source code of Java String Builder
         StringBuilder sb = new StringBuilder();
     }
+
+    // also in the Abstract Factory, in fact there could be a nested builder pattern in the motor build example
 }
