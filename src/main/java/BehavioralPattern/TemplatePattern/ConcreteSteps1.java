680fc6f394f37a3ea27da9fe2ff2d09ec628b275
Xiaoming Sun <rsun2014@gmail.com>
2018-03-12 10:39:35 -0700

    Template Pattern

---
+++ src/main/java/BehavioralPattern/TemplatePattern/ConcreteSteps1.java
@@ -0,0 +1,19 @@
+package BehavioralPattern.TemplatePattern;
+
+public class ConcreteSteps1 extends Template {
+
+    @Override
+    public void step1() {
+        System.out.println("Step 1 in ConcreteSteps1");
+    }
+
+    @Override
+    public void step2() {
+        System.out.println("Step 2 in ConcreteSteps1");
+    }
+
+    @Override
+    public void step3() {
+        System.out.println("Step 3 in ConcreteSteps1");
+    }
+}
