89fe1c749dde592adb9344745a4ff8bf2534b966
Xiaoming Sun <rsun2014@gmail.com>
2018-07-27 09:18:35 -0700

    comments for template pattern

--- src/main/java/behavioralpattern/templatepattern/Template.java
+++ src/main/java/behavioralpattern/templatepattern/Template.java
@@ -1,12 +1,17 @@
 package behavioralpattern.templatepattern;
 
 public abstract class Template {
+    // This is the template method
+    // which won't be override by subclasses
+    // It defines the steps
     public void runIt() {
         step1();
         step2();
         step3();
     }
 
+    // These are step method,
+    // which should be override by subclasses
     public abstract void step1();
     public abstract void step2();
     public abstract void step3();
